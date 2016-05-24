a71026128b4eef474a94a72148325579a719ef54
BlueDragon23 <aidan_goldthorpe@outlook.com>
2016-05-24 19:23:02 +1000

    Added program to manage data importing

---
+++ data.py
@@ -0,0 +1,36 @@
+import json
+
+
+def read_json():
+    """
+    Read the file, and convert the json to a python format
+    :return:
+    """
+    file = open("C:\\Users\\Aidan\\Dropbox\\University\\COSC3000\ComputerGraphics\\cards.collectible.json")
+
+    cards = json.load(file)
+
+    return cards
+
+def get_minions(cards) -> []:
+    """
+    Read data from the card data file. Returns a list of (mana, health, attack)
+    """
+    keys = ["cost", "health", "attack", "name", "text"]
+    values = [{key: card.get(key, "") for key in keys} for card in cards if card['type'] == 'MINION']
+    return values
+
+
+def get_spells(cards):
+    """
+    :return: all spells in the game
+    """
+    keys = ["name", "cost", "text"]
+    spells = [{key: card[key] for key in keys} for card in cards if card['type'] == 'SPELL']
+    return spells
+
+if __name__ == "__main__":
+    cards = read_json()
+    minions = get_minions(cards)
+    for i in range(5):
+        print(minions[i])
