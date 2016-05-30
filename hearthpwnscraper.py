e6332be982d298464e16d6e9261aca120b0c5585
BlueDragon23 <aidan_goldthorpe@outlook.com>
2016-05-31 02:26:27 +1000

    Changed to scrape abilities (slowly)

--- hearthpwnscraper.py
+++ hearthpwnscraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import shutil
 
 for i in range(1, 7):
-    url = "http://www.hearthpwn.com/cards/minion?display=3&filter-premium=1&page={}".format(i)
+    url = "http://www.hearthpwn.com/cards/ability?display=3&filter-premium=1&page={}".format(i)
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
     for link in soup.find_all('img'):
