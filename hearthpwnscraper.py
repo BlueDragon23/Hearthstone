fcb2b2b442b872195855655fb0582fad07b96d52
BlueDragon23 <aidan_goldthorpe@outlook.com>
2016-06-02 20:39:08 +1000

    Improved the scraper to download both minions and spells when run

--- hearthpwnscraper.py
+++ hearthpwnscraper.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import shutil
 
-for i in range(1, 7):
-    url = "http://www.hearthpwn.com/cards/ability?display=3&filter-premium=1&page={}".format(i)
+
+def download_images(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
     for link in soup.find_all('img'):
@@ -12,3 +12,12 @@ for i in range(1, 7):
             with open(link.get('data-href')[1:] + ".png", 'wb') as f:
                 pic.raw.decode_content = True
                 shutil.copyfileobj(pic.raw, f)
+
+
+if __name__ == "__main__":
+    for i in range(1, 7):
+        url = "http://www.hearthpwn.com/cards/minion?display=3&filter-premium=1&page={}".format(i)
+        download_images(url)
+    for i in range(1, 3):
+        url = "http://www.hearthpwn.com/cards/ability?display=3&filter-premium=1&page={}".format(i)
+        download_images(url)
